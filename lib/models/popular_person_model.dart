2b778d9c6b91077b16aada4c90071d69748e0c59
Amira1419 <amiraabdelsalam98@yahoo.com>
2022-06-28 16:07:37 +0200

    make model for popular person

---
+++ lib/models/popular_person_model.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+class PopularPerson{
+  final int id;
+  final String department;
+  final String name;
+  final String profilePath;
+
+  PopularPerson({
+    required this.id,
+    required this.department,
+    required this.name,
+    required this.profilePath
+  });
+
+  PopularPerson.fromJson(Map<String, dynamic> json):
+  id = json['id'],
+  name = json['name'],
+  department = json['known_for_department'],
+  profilePath = json['profile_path'];
+}
